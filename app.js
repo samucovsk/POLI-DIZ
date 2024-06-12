7787bc5bfc83391d32b861e3d7993ae5f8356e78
samucovsk <samufrrira@gmail.com>
2024-06-12 11:03:11 -0300

    aa

--- app.js
+++ app.js
@@ -12,8 +12,8 @@ app.set("views", "./app/views");
 app.use(express.json()); 
 app.use(express.urlencoded({ extended: true }));
 
-var rotas = require("./app/routes/router");
-app.use("/", rotas);
+// var rotas = require("./app/routes/router");
+// app.use("/", rotas);
 
 app.listen(port, () => {
   console.log(`Servidor ouvindo na porta ${port}\nhttp://localhost:${port}`);
