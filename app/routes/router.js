bd8f550e38714b64514bef1e54abdf282360389e
samucovsk <samufrrira@gmail.com>
2024-06-13 16:01:15 -0300

    aa

--- app/routes/router.js
+++ app/routes/router.js
@@ -1,3 +1,12 @@
+router.get("/home", function (req, res) {
+    res.render("views/pages/index.html",{pagina:"home"});
+});
 router.get("/login", function (req, res) {
-    res.render("views/pages/index.html",{pagina:"login"});
+    res.render("views/pages/login.html",{pagina:"login"});
+});
+router.get("/noticia", function (req, res) {
+    res.render("views/pages/news.html",{pagina:"noticias"});
+});
+router.get("/politicos", function (req, res) {
+    res.render("views/pages/perfil-candidato.html",{pagina:"politicos"});
 });
