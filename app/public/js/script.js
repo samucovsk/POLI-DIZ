d4a717419dc756b9b195e89f57b5f5c9b671cb1f
samucovsk <samufrrira@gmail.com>
2024-06-13 14:20:36 -0300

    vamoqvamo

--- app/public/js/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const menuHamburguer = document.querySelector('.menu-hamburguer');
-    const menu = document.querySelector('.menu');
-
-    menuHamburguer.addEventListener('click', () => {
-        menu.classList.toggle('aberto');
-    });
-});
