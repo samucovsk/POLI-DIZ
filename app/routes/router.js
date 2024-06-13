3159d084739c7019f83fcd8c4b14402230366a5f
samucovsk <samufrrira@gmail.com>
2024-06-13 14:29:33 -0300

    asas

--- app/routes/router.js
+++ app/routes/router.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var router = express.Router();
 
-router.get("/", function (req, res) {
+router.get("/login", function (req, res) {
     res.render("C:\Users\PMB\Downloads\POLI-DEZ\app\views\pages\login.html",{pagina:"login"});
 });
