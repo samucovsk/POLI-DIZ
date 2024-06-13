95cfc0a9747b84a21f70fc4380b14f6f8eb587c9
samucovsk <samufrrira@gmail.com>
2024-06-13 14:27:34 -0300

    a

--- app/routes/router.js
+++ app/routes/router.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var router = express.Router();
 
-router.get("/login.html", function (req, res) {
+router.get("/", function (req, res) {
     res.render("app\views\pages\login.html",{pagina:"login"});
 });
