10412b67ddc7c77460c6ae39d1b1828c53500286
samucovsk <samufrrira@gmail.com>
2024-06-18 16:11:32 -0300

    aaaa

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.set("views", "./app/views");
 app.use(express.json()); 
 app.use(express.urlencoded({ extended: true }));
 
-var rotas = require("routes/router");
+var rotas = require("app\routes\router.js");
 app.use("/", rotas);
 
 app.listen(port, () => {
