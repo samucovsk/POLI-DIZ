ef4b26e48913b43b41572384d734d6b12a91fb9d
samucovsk <samufrrira@gmail.com>
2024-06-18 11:00:57 -0300

    aasas

--- app/routes/router.js
+++ app/routes/router.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/home", function (req, res) {
-    res.render("pages/index");
+router.get("/", function (req, res) {
+    res.render("pages/home", {pagina:"home", logado:null});
 });
 router.get("/login", function (req, res) {
     res.render("pages/login.html");
