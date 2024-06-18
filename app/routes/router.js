cc9ba18ae3a8d50e715fa297a3823328938fdd9e
samucovsk <samufrrira@gmail.com>
2024-06-18 10:49:37 -0300

    meu deus meu senhor me ajuda por favor

--- app/routes/router.js
+++ app/routes/router.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const router = express.Router();
 
-const pool = require("../config/pool-conexoes");
-
 router.get("/home", function (req, res) {
     res.render("pages/index.html");
 });
