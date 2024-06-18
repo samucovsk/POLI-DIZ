5bfd52e5f8007e0da20fb28e7611fb77985806de
samucovsk <samufrrira@gmail.com>
2024-06-18 14:48:40 -0300

    s

--- routes/router.js
+++ routes/router.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", function (req, res) {
-    res.render("pages/index", {pagina:"home", logado:null});
+    res.render("app/public/pages/index", {pagina:"home", logado:null});
 });
 router.get("/login", function (req, res) {
     res.render("pages/login.html");
