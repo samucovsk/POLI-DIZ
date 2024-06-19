f1b890720dd63323bf8b4712fef8ddfa6e79aeac
samucovsk <samufrrira@gmail.com>
2024-06-19 10:49:35 -0300

    obrigado professoooor

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.set("views", "./app/views");
 app.use(express.json()); 
 app.use(express.urlencoded({ extended: true }));
 
-var rotas = require("app\routes\router.js");
+var rotas = require("./app/routes/router");
 app.use("/", rotas);
 
 app.listen(port, () => {
