74f3b14d79bbaef4a3380ff4d84ce048b36186a3
samucovsk <samufrrira@gmail.com>
2024-06-19 11:05:53 -0300

    samuca

--- app/routes/router.js
+++ app/routes/router.js
@@ -5,13 +5,13 @@ router.get("/", function (req, res) {
     res.render("pages/index", {pagina:"home", logado:null});
 });
 router.get("/login", function (req, res) {
-    res.render("pages/login");
+    res.render("pages/login", {pagina:"login", logado:null});
 });
 router.get("/noticias", function (req, res) {
-    res.render("pages/news");
+    res.render("pages/news", {pagina:"noticia", logado:null});
 });
 router.get("/politicos", function (req, res) {
-    res.render("pages/perfil-candidato");
+    res.render("pages/perfil-candidato", {pagina:"politicos", logado:null});
 });
 
 module.exports = router;
