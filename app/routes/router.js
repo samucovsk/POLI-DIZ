ed73c7aaee1752199385beac96598671d49335e6
samucovsk <samufrrira@gmail.com>
2024-06-20 11:36:50 -0300

    geo

--- app/routes/router.js
+++ app/routes/router.js
@@ -19,4 +19,8 @@ router.get("/politicos", function (req, res) {
     res.render("pages/PARTIDOS", {pagina:"politicos", logado:null});
 });
 
+router.get("/crieuma", function (req, res) {
+    res.render("pages/escolha", {pagina:"politicos", logado:null});
+});
+
 module.exports = router;
