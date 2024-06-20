319b9cf81383d7b85913007c74a2460ded41d76c
samucovsk <samufrrira@gmail.com>
2024-06-20 07:52:56 -0300

    aa

--- app/routes/router.js
+++ app/routes/router.js
@@ -4,6 +4,11 @@ const router = express.Router();
 router.get("/", function (req, res) {
     res.render("pages/index", {pagina:"home", logado:null});
 });
+
+router.get("/home", function (req, res) {
+    res.render("pages/index", {pagina:"home", logado:null});
+});
+
 router.get("/login", function (req, res) {
     res.render("pages/login", {pagina:"login", logado:null});
 });
