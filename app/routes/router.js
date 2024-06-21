06e8421c910cff6532e7458c11acd47639320e5f
samucovsk <samufrrira@gmail.com>
2024-06-21 16:39:24 -0300

    sasasasasasasas

--- app/routes/router.js
+++ app/routes/router.js
@@ -9,8 +9,8 @@ router.get("/home", function (req, res) {
     res.render("pages/index", {pagina:"home", logado:null});
 });
 
-router.get("/login", function (req, res) {
-    res.render("pages/login", {pagina:"login", logado:null});
+router.get("/escolha", function (req, res) {
+    res.render("pages/escolha", {pagina:"escolha", logado:null});
 });
 router.get("/noticias", function (req, res) {
     res.render("pages/news", {pagina:"noticias", logado:null});
@@ -18,15 +18,9 @@ router.get("/noticias", function (req, res) {
 router.get("/politicos", function (req, res) {
     res.render("pages/PARTIDOS", {pagina:"politicos", logado:null});
 });
-
-router.get("/escolha", function (req, res) {
-    res.render("pages/escolha", {pagina:"escolha", logado:null});
+router.get("/usuario", function (req, res) {
+    res.render("pages/cadastro-usuario", {pagina:"usuario", logado:null});
 });
-
-router.get("/cadastro", function (req, res) {
-    res.render("pages/cadastro-usuario", {pagina:"cadastro", logado:null});
-});
-
 
 
 module.exports = router;
